73b07da5f1bb879625bf054d8e5a727d045bb149
RDN (Mac) <desmond.ngueguin@gmail.com>
2023-01-30 00:50:32 +0000

    Moved notebooks to Visual Fluids - VFX

--- examples/_utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from .plotting import *
-from .dataloading import *
--- updec/utils.py
+++ updec/utils.py
@@ -1,10 +1,11 @@
 import jax
 import jax.numpy as jnp
-import math
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+import math
 from functools import partial
-# import matplotlib.pyplot as plt
-# import seaborn as sns
+
 
 
 ## Euclidian distance
@@ -74,37 +75,28 @@ def make_monomial(x, id):
     else:
         pass        ## Higher order monomials not yet supported
 
-    # monomials = [lambda x:1.0,
-    #             lambda x:x[0],
-    #             lambda x:x[1],
-    #             lambda x:x[0]**2,
-    #             lambda x:x[0]*x[1],
-    #             lambda x:x[1]**2]
-    # return jax.lax.switch(id, jnp.array(monomials))(x)
-
 
 def compute_nb_monomials(max_degree, problem_dimension):
     return math.comb(max_degree+problem_dimension, max_degree)
 
 
-# # plt.style.use('bmh')
+# plt.style.use('bmh')
+sns.set(context='notebook', style='ticks',
+        font='sans-serif', font_scale=1, color_codes=True, rc={"lines.linewidth": 2})
 
-# sns.set(context='notebook', style='ticks',
-#         font='sans-serif', font_scale=1, color_codes=True, rc={"lines.linewidth": 2})
-
-# ## Wrapper function for matplotlib and seaborn
-# def plot(*args, ax=None, figsize=(6,3.5), x_label=None, y_label=None, title=None, y_scale='linear', **kwargs):
-#     if ax==None: 
-#         _, ax = plt.subplots(1, 1, figsize=figsize)
-#     # sns.despine(ax=ax)
-#     if x_label:
-#         ax.set_xlabel(x_label)
-#     if y_label:
-#         ax.set_ylabel(y_label)
-#     if title:
-#         ax.set_title(title)
-#     ax.plot(*args, **kwargs)
-#     ax.set_yscale(y_scale)
-#     ax.legend()
-#     plt.tight_layout()
-#     return ax
+## Wrapper function for matplotlib and seaborn
+def plot(*args, ax=None, figsize=(6,3.5), x_label=None, y_label=None, title=None, y_scale='linear', **kwargs):
+    if ax==None: 
+        _, ax = plt.subplots(1, 1, figsize=figsize)
+    # sns.despine(ax=ax)
+    if x_label:
+        ax.set_xlabel(x_label)
+    if y_label:
+        ax.set_ylabel(y_label)
+    if title:
+        ax.set_title(title)
+    ax.plot(*args, **kwargs)
+    ax.set_yscale(y_scale)
+    ax.legend()
+    plt.tight_layout()
+    return ax
